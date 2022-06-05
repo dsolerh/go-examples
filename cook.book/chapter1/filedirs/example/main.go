120adb256fe8a2e183002933e0d021c89fd5a281
dsolerh <dsolerh.cinter95@gmail.com>
2022-06-05 19:27:01 -0400

    work with files and dir

---
+++ cook.book/chapter1/filedirs/example/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "github.com/dsolerh/examples/cook.book/chapter1/filedirs"
+
+func main() {
+	if err := filedirs.Operate(); err != nil {
+		panic(err)
+	}
+	if err := filedirs.CapitalizerExample(); err != nil {
+		panic(err)
+	}
+}
