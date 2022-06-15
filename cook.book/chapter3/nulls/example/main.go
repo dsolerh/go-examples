68ca7b603535033dbcf41fffadba45ec42ec7e15
dsolerh <dsolerh.cinter95@gmail.com>
2022-06-15 17:31:25 -0400

    null is also valid

---
+++ cook.book/chapter3/nulls/example/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/dsolerh/examples/cook.book/chapter3/nulls"
+)
+
+func main() {
+	if err := nulls.BaseEncoding(); err != nil {
+		panic(err)
+	}
+	fmt.Println()
+	if err := nulls.PointerEncoding(); err != nil {
+		panic(err)
+	}
+	fmt.Println()
+	if err := nulls.NullEncoding(); err != nil {
+		panic(err)
+	}
+}
