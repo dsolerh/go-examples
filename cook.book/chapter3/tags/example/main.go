b29c48477c468158997ab6deeac01157901d23d5
dsolerh <dsolerh.cinter95@gmail.com>
2022-06-16 16:06:40 -0400

    serialization and tags

---
+++ cook.book/chapter3/tags/example/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/dsolerh/examples/cook.book/chapter3/tags"
+)
+
+func main() {
+	if err := tags.EmptyStruct(); err != nil {
+		panic(err)
+	}
+	fmt.Println()
+	if err := tags.FullStruct(); err != nil {
+		panic(err)
+	}
+}
