d56d5844513f7670e75bc7ab92577b1dca8d04e1
dsolerh <dsolerh.cinter95@gmail.com>
2022-06-18 07:33:53 -0400

    strctured logs

---
+++ cook.book/chapter4/structured/example/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/dsolerh/examples/cook.book/chapter4/structured"
+)
+
+func main() {
+	fmt.Println("Logrus:")
+	structured.Logrus()
+	fmt.Println()
+	fmt.Println("Apex:")
+	structured.Apex()
+}
