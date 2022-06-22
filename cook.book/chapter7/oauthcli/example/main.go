d2447d5cd0409e7d28dee16a0cd000f57fbe25e3
dsolerh <dsolerh.cinter95@gmail.com>
2022-06-22 16:36:53 -0400

    nice oauth

---
+++ cook.book/chapter7/oauthcli/example/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"context"
+
+	"github.com/dsolerh/examples/cook.book/chapter7/oauthcli"
+)
+
+func main() {
+	ctx := context.Background()
+	conf := oauthcli.Setup()
+	tok, err := oauthcli.GetToken(ctx, conf)
+	if err != nil {
+		panic(err)
+	}
+	client := conf.Client(ctx, tok)
+	if err := oauthcli.GetUser(client); err != nil {
+		panic(err)
+	}
+}
