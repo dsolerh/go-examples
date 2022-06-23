8f4c385d7aa08bc05106dc3066e05cbc992baa23
dsolerh <dsolerh.cinter95@gmail.com>
2022-06-23 12:59:57 -0400

    grpc not working

---
+++ cook.book/chapter7/grpc/server/server.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net"
+
+	"github.com/dsolerh/examples/cook.book/chapter7/grpc/greeter"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	grpcServer := grpc.NewServer()
+	greeter.RegisterGreeterServiceServer(grpcServer,
+		&Greeter{Exclaim: true})
+	lis, err := net.Listen("tcp", ":4444")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Listening on port :4444")
+	grpcServer.Serve(lis)
+}
