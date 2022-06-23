308f21a5cef63b5723554e27c2ed611769c82df9
dsolerh <dsolerh.cinter95@gmail.com>
2022-06-23 13:24:55 -0400

    a simple server

---
+++ cook.book/chapter8/handlers/example/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/dsolerh/examples/cook.book/chapter8/handlers"
+)
+
+func main() {
+	http.HandleFunc("/name", handlers.HelloHandler)
+	http.HandleFunc("/greeting", handlers.GreetingHandler)
+	fmt.Println("Listening on port :3333")
+	log.Fatal(http.ListenAndServe(":3333", nil))
+}
