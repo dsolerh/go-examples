6ce2bd850b811db1271353076ae892ee36249371
dsolerh <dsolerh.cinter95@gmail.com>
2022-06-26 08:50:13 -0400

    add adapter

---
+++ algorithms.book/chapter1/structural/adapter/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+//IProcess interface
+type IProcess interface {
+	process()
+}
+
+//Adapter struct
+type Adapter struct {
+	adaptee Adaptee
+}
+
+//Adapter class method process
+func (adapter Adapter) process() {
+	fmt.Println("Adapter process")
+	adapter.adaptee.convert()
+}
+
+//Adaptee Struct
+type Adaptee struct{}
+
+// Adaptee class method convert
+func (adaptee Adaptee) convert() {
+	fmt.Println("Adaptee convert method")
+}
+
+// main method
+func main() {
+	var processor IProcess = Adapter{}
+	processor.process()
+}
