b14b7305a8ce85ec0d42d308f29c14a65825ce37
dsolerh <dsolerh.cinter95@gmail.com>
2022-10-19 15:27:16 +0300

    update app

--- concurrency.course/finalProject/cmd/web/mailer.go
+++ concurrency.course/finalProject/cmd/web/mailer.go
@@ -27,14 +27,15 @@ type MailServer struct {
 }
 
 type Message struct {
-	From        string
-	FromName    string
-	To          string
-	Subject     string
-	Attachments []string
-	Data        any
-	DataMap     map[string]any
-	Template    string
+	From          string
+	FromName      string
+	To            string
+	Subject       string
+	Attachments   []string
+	AttachmentMap map[string]string
+	Data          any
+	DataMap       map[string]any
+	Template      string
 }
 
 // a function to listn for messages
@@ -66,10 +67,14 @@ func (m *MailServer) sendMail(msg Message, errorChan chan error) {
 		msg.FromName = m.FromName
 	}
 
-	data := map[string]any{
-		"message": msg.Data,
+	if msg.AttachmentMap == nil {
+		msg.AttachmentMap = make(map[string]string)
 	}
-	msg.DataMap = data
+
+	if len(msg.DataMap) == 0 {
+		msg.DataMap = make(map[string]any)
+	}
+	msg.DataMap["message"] = msg.Data
 
 	// build the html message
 	formattedMessage, err := m.buildHTMLMessage(msg)
@@ -108,6 +113,12 @@ func (m *MailServer) sendMail(msg Message, errorChan chan error) {
 	if len(msg.Attachments) > 0 {
 		for _, x := range msg.Attachments {
 			email.AddAttachment(x)
+		}
+	}
+
+	if len(msg.AttachmentMap) > 0 {
+		for key, value := range msg.AttachmentMap {
+			email.AddAttachment(value, key)
 		}
 	}
 
