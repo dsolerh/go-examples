8e09bd28418936335628003581a2035ae1d34756
dsolerh <dsolerh.cinter95@gmail.com>
2024-06-08 23:57:46 +0300

    chore: update the wc tool

--- cli_projects/wc/main.go
+++ cli_projects/wc/main.go
@@ -2,18 +2,25 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"io"
 	"os"
 )
 
 func main() {
-	fmt.Println(count(os.Stdin))
+	lines := flag.Bool("l", false, "count lines")
+	flag.Parse()
+	fmt.Println(count(os.Stdin, *lines))
 }
 
-func count(r io.Reader) int {
+func count(r io.Reader, lines bool) int {
 	scanner := bufio.NewScanner(r)
-	scanner.Split(bufio.ScanWords)
+	if lines {
+		scanner.Split(bufio.ScanLines)
+	} else {
+		scanner.Split(bufio.ScanWords)
+	}
 	// create a variable to count the words
 	wc := 0
 	for scanner.Scan() {
--- cli_projects/wc/main_test.go
+++ cli_projects/wc/main_test.go
@@ -7,26 +7,37 @@ import (
 
 func TestCountWords(t *testing.T) {
 	type Case struct {
-		str string
-		exp int
+		str   string
+		lines bool
+		exp   int
 	}
 
 	type testCase map[string]Case
 
 	tests := testCase{
-		"one line": Case{
+		"count words: one line": Case{
 			str: "one two three",
 			exp: 3,
 		},
-		"multi lines": Case{
+		"count words: multi lines": Case{
 			str: "onw\ndd\nqwer\ndas",
 			exp: 4,
+		},
+		"count lines: one line": Case{
+			str:   "one two three",
+			lines: true,
+			exp:   1,
+		},
+		"count lines: multi lines": Case{
+			str:   "onw\ndd\nqwer\ndas",
+			lines: true,
+			exp:   4,
 		},
 	}
 	for tname, tcase := range tests {
 		t.Run(tname, func(t *testing.T) {
 			b := bytes.NewBufferString(tcase.str)
-			got := count(b)
+			got := count(b, tcase.lines)
 
 			if got != tcase.exp {
 				t.Errorf("got != from expected (%d!=%d)", got, tcase.exp)
